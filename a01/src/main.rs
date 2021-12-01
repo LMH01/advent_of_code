8e1a228cf572a2143675a1093566286ed20d1742
LMH01 <louis.haensch01@gmail.com>
2021-12-01 11:38:20 +0100

    completed a01

---
+++ a01/src/main.rs
@@ -0,0 +1,19 @@
+use std::i32::MAX;
+
+use adventofcode_lmh01_lib::read_file;
+
+fn main() {
+   let content = read_file("input.txt");
+   let mut x;
+   let mut y = MAX;
+   let mut increases = 0;
+   for i in content.iter() {
+       x = i.to_string().parse::<i32>().unwrap();
+       if x > y {
+           increases+=1;
+       }
+       y = x;
+       println!("{}", i);
+   }
+   println!("Total increases: {}", increases);
+}
