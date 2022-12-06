b6ebd5506b51e2646bd5596b1ab521da75a9d69a
LMH01 <louis.haensch01@gmail.com>
2022-12-06 20:10:46 +0100

    2022 Day06 Part2

--- src/years/y2022/day06.rs
+++ src/years/y2022/day06.rs
@@ -5,23 +5,25 @@ use miette::Result;
 
 pub fn part1(_debug: bool) -> Result<()> {
     let content = read_file("input/y2022/day06.txt")?;
-    let p_start = package_start(content.get(0).unwrap());
+    let p_start = package_start(content.get(0).unwrap(), 4);
     println!("Package start after at char {}", p_start);
     Ok(())
 }
 
 pub fn part2(_debug: bool) -> Result<()> {
     let content = read_file("input/y2022/day06.txt")?;
+    let p_start = package_start(content.get(0).unwrap(), 14);
+    println!("Package start after at char {}", p_start);
     Ok(())
 }
 
-fn package_start(input: &String) -> i32 {
+fn package_start(input: &String, header_length: usize) -> i32 {
     let mut chars = 0;
     let mut latest_chars = VecDeque::new();
     for c in input.chars() {
         chars += 1;
         latest_chars.push_back(c); 
-        if latest_chars.len() == 4 {
+        if latest_chars.len() == header_length {
             if unique(&latest_chars) {
                 return chars;
             }
