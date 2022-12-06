6ba3005661fae8d40c11318d6223b389b706eea0
LMH01 <louis.haensch01@gmail.com>
2022-12-06 22:01:28 +0100

    2022 Day04 Part2

--- src/years/y2022/day04.rs
+++ src/years/y2022/day04.rs
@@ -3,6 +3,17 @@ use miette::Result;
 
 pub fn part1(_debug: bool) -> Result<()> {
     let content = read_file("input/y2022/day04.txt")?;
+    calculate(content, true);
+    Ok(())
+}
+
+pub fn part2(_debug: bool) -> Result<()> {
+    let content = read_file("input/y2022/day04.txt")?;
+    calculate(content, false);
+    Ok(())
+}
+
+fn calculate(content: Vec<String>, part1: bool) {
     let mut i = 0;
     for line in content {
         let numbers = numbers_from_string(&line);
@@ -12,23 +23,30 @@ pub fn part1(_debug: bool) -> Result<()> {
         let b0 = numbers[2];
         let b1 = numbers[3];
         let b: (u32, u32) = (b0, b1);
-        if fully_contained(a, b) {
-            i += 1;
+        if part1 {
+            if fully_contained(a, b) {
+                i += 1;
+            }
+        } else {
+            if overlaps(a, b) {
+                i += 1;
+            }
         }
     }
     println!("To reconsider: {i}");
-    Ok(())
-}
-
-pub fn part2(_debug: bool) -> Result<()> {
-    let content = read_file("input/y2022/day04.txt")?;
-    Ok(())
 }
 
 fn fully_contained(a: (u32, u32), b: (u32, u32)) -> bool {
     if a.0 >= b.0 && a.1 <= b.1 {
         return true;
     } else if b.0 >= a.0 && b.1 <= a.1 {
+        return true;
+    }
+    false
+}
+
+fn overlaps(a: (u32, u32), b: (u32, u32)) -> bool {
+    if a.1 >= b.0 && a.0 <= b.1 {
         return true;
     }
     false
