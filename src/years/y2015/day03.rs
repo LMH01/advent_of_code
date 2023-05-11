d29d2be48e8bf0c450dd0bb5e1842f972abe693e
LMH01 <louis.haensch01@gmail.com>
2023-05-11 13:41:41 +0200

    2015 Day03 Part2

--- src/years/y2015/day03.rs
+++ src/years/y2015/day03.rs
@@ -5,12 +5,34 @@ use miette::Result;
 
 pub fn part1(debug: bool) -> Result<()> {
     let content = read_file("input/y2015/day03.txt")?;
-    let mut visited_coordinats: HashSet<(i32, i32)> = HashSet::new();
-    let line = &content[0];
+    let mut visited_coordinates: HashSet<(i32, i32)> = HashSet::new();
+    visit_houses(&content[0], &mut visited_coordinates);
+    println!("duplicate visits: {}", visited_coordinates.len());
+    Ok(())
+}
+
+pub fn part2(debug: bool) -> Result<()> {
+    let content = read_file("input/y2015/day03.txt")?;
+    let mut visited_coordinates: HashSet<(i32, i32)> = HashSet::new();
+    let mut instructions_1 = String::new();
+    let mut instructions_2 = String::new();
+    for (i, c) in content[0].chars().enumerate() {
+        if i%2 == 0 {
+            instructions_1.push(c);
+        } else {
+            instructions_2.push(c);
+        }
+    }
+    visit_houses(&instructions_1, &mut visited_coordinates);
+    visit_houses(&instructions_2, &mut visited_coordinates);
+    println!("duplicate visits: {}", visited_coordinates.len());
+    Ok(())
+}
+
+fn visit_houses(instructions: &str, visited_coordinates: &mut HashSet<(i32, i32)>) {
     let mut x = 0;
     let mut y = 0;
-    let mut duplicates = 0;
-    for c in line.chars() {
+    for c in instructions.chars() {
         match c {
             '^' => y+=1,
             '<' => x-=1,
@@ -18,15 +40,6 @@ pub fn part1(debug: bool) -> Result<()> {
             'v' => y-=1,
             _ => (),
         }
-        if visited_coordinats.insert((x, y)) {
-            duplicates += 1;
-        }
+        visited_coordinates.insert((x, y));
     }
-    println!("duplicate visits: {duplicates}");
-    Ok(())
-}
-
-pub fn part2(debug: bool) -> Result<()> {
-    let content = read_file("input/y2015/day03.txt")?;
-    Ok(())
-}
+} 
