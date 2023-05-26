4fe19fd3ab8fe84ffa02b747149138dc24cad300
LMH01 <louis.haensch01@gmail.com>
2023-05-26 18:07:48 +0200

    2015 Day07 Part1

--- src/years/y2015/day07.rs
+++ src/years/y2015/day07.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::{HashSet, HashMap, LinkedList}, clone, thread, time::Duration};
 
 use adventofcode_lmh01_lib::read_file;
 use miette::Result;
@@ -7,13 +7,75 @@ pub fn part1(_debug: bool) -> Result<()> {
     let content = read_file("input/y2015/day07.txt")?;
     let mut instructions = Vec::new();
     for line in content {
-        instructions.push(Instruction::from_str(&line));
+        instructions.push(Instruction::from_str(&line).unwrap());
     }
     // Load values
-    //let mut 
-    //for instruction in instructions {
+    let mut available_values = HashMap::new();
+    let mut open_instructions = LinkedList::new();
+    for instruction in instructions {
+        match instruction {
+            Instruction::LoadValue(v, k) => {
+                available_values.insert(k, v);
+            },
+            _ => open_instructions.push_back(instruction),
+        };
+    }
 
-    //}
+    // Update available values for each open instruction
+    while !open_instructions.is_empty() {
+        let current = open_instructions.pop_front().unwrap();
+        match current.clone() {
+            Instruction::And(x, y, z) => {
+                if let Some(v) = get_value(Operation::And, &x, &y, &available_values) {
+                    available_values.insert(z, v);
+                } else {
+                    open_instructions.push_back(current);
+                }
+            }
+            Instruction::Or(x, y, z) => {
+                if let Some(v) = get_value(Operation::Or, &x, &y, &available_values) {
+                    available_values.insert(z, v);
+                } else {
+                    open_instructions.push_back(current);
+                }
+            }
+            Instruction::LShift(x, y, z) => {
+                if available_values.contains_key(&x) {
+                    let v = available_values.get(&x).unwrap() << y;
+                    available_values.insert(z, v);
+                } else {
+                    open_instructions.push_back(current);
+                }
+            }
+            Instruction::RShift(x, y, z) => {
+                if available_values.contains_key(&x) {
+                    let v = available_values.get(&x).unwrap() >> y;
+                    available_values.insert(z, v);
+                } else {
+                    open_instructions.push_back(current);
+                }
+            }
+            Instruction::Not(x, y) => {
+                if available_values.contains_key(&x) {
+                    let v = !available_values.get(&x).unwrap();
+                    available_values.insert(y, v);
+                } else {
+                    open_instructions.push_back(current);
+                }
+            }
+            Instruction::Redirect(x, y) => {
+                if available_values.contains_key(&x) {
+                    let v = available_values.get(&x).unwrap();
+                    available_values.insert(y, *v);
+                } else {
+                    open_instructions.push_back(current);
+                }
+            }
+            _ => (),
+        }
+        println!("Open instruction length: {}", open_instructions.len());
+    }
+    println!("Value of a: {}", available_values.get("a").expect("Not found!"));
     Ok(())
 }
 
@@ -22,9 +84,46 @@ pub fn part2(_debug: bool) -> Result<()> {
     Ok(())
 }
 
+/// Tries to parse a new value:
+/// 
+/// Checks if `v1` and `v2`s values are contained in `available_values`.
+/// If values are contained, these values will be used to calculate the new value.
+/// If the values are not contained, it will be tried to parse them into u16.
+/// If that succeeds, these values are used to calculate the new value.
+/// If nothing works, `None` is returned.
+fn get_value(operation: Operation, v1: &str, v2: &str, available_values: &HashMap<String, u16>) -> Option::<u16> {
+    if v1.parse::<u16>().is_ok() && v2.parse::<u16>().is_ok() {
+        return Some(operation.calculate(&v1.parse::<u16>().unwrap(), &v2.parse::<u16>().unwrap()));
+    } else if v1.parse::<u16>().is_ok() && available_values.contains_key(v2) {
+        return Some(operation.calculate(&v1.parse::<u16>().unwrap(), available_values.get(v2).unwrap()));
+    } else if available_values.contains_key(v1) && v2.parse::<u16>().is_ok() {
+        return Some(operation.calculate(available_values.get(v1).unwrap(), &v1.parse::<u16>().unwrap()))
+    } else if available_values.contains_key(v1) && available_values.contains_key(v2) {
+        return Some(operation.calculate(available_values.get(v1).unwrap(), available_values.get(v2).unwrap()));
+    }
+    None
+}
+
+enum Operation {
+    And,
+    Or,
+}
+
+impl Operation {
+    fn calculate(&self, v1: &u16, v2: &u16) -> u16 {
+        match self {
+            Operation::And => v1 & v2,
+            Operation::Or => v1 | v2,
+        }
+    }
+}
+
+#[derive(Clone)]
 enum Instruction {
     /// 123 -> x
-    Load(u16, String),
+    LoadValue(u16, String),
+    /// x -> y
+    Redirect(String, String),
     /// x AND y -> z
     And(String, String, String),
     /// x OR y -> Z
@@ -44,9 +143,9 @@ impl Instruction {
             // Instruction is load
             let num = splits[0].parse();
             if num.is_err() {
-                return Err(format!("Unable to parse input value: {}", num.unwrap_err()));
+                return Ok(Instruction::Redirect(String::from(splits[0]), String::from(splits[2])));
             }
-            return Ok(Instruction::Load(num.unwrap(), String::from(splits[2])));
+            return Ok(Instruction::LoadValue(num.unwrap(), String::from(splits[2])));
         } else if splits.len() == 4 {
             // instruction is NOT
             return Ok(Instruction::Not(String::from(splits[1]), String::from(splits[3])));
