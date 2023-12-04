0771ed9709887683330766c01ba9137b98979bb2
LMH01 <louis.haensch01@gmail.com>
2023-12-04 06:59:50 +0100

    y2023 d04 restructureing

--- src/years/y2023/day04.rs
+++ src/years/y2023/day04.rs
@@ -7,23 +7,7 @@ pub fn part1(_debug: bool) -> Result<()> {
     let content = read_file("input/y2023/day04.txt")?;
     let mut points = 0;
     for mut line in content {
-        // remove duplicated whitespaces (otherwise empty strings may be contained in the vector)
-        line = line.replace("   ", " ");
-        line = line.replace("  ", " ");
-        let chunks = line.split(' ').collect::<Vec<&str>>();
-        let mut winning_numbers = HashSet::new();
-        let mut owned_numbers = HashSet::new();
-        // filter out empty strings
-
-        // construct sets
-        for i in 2..chunks.len() {
-            // we skip the first two chunks because they are something like //Card 1:
-            if (2..=11).contains(&i) {
-                winning_numbers.insert(chunks[i].parse::<u32>().unwrap());
-            } else if (13..=37).contains(&i) {
-                owned_numbers.insert(chunks[i].parse::<u32>().unwrap());
-            }
-        }
+        let (winning_numbers, owned_numbers) = construct_sets(line, false);
         // check matches
         let mut card_points = 0;
         for i in owned_numbers {
@@ -36,7 +20,6 @@ pub fn part1(_debug: bool) -> Result<()> {
                 card_points = card_points*2;
             }
         }
-        println!("Card points: {card_points}");
         points += card_points;
     }
     println!("Total points: {points}");
@@ -44,8 +27,53 @@ pub fn part1(_debug: bool) -> Result<()> {
 }
 
 pub fn part2(_debug: bool) -> Result<()> {
-    let content = read_file("input/y2023/day04.txt")?;
+    let content = read_file("input/y2023/day04-demo.txt")?;
+    let mut total_matches = 0;
+    for mut line in content {
+        let (winning_numbers, owned_numbers) = construct_sets(line, true);
+        
+        // check matches
+        let mut card_matches = 0;
+        for i in owned_numbers {
+            if !winning_numbers.contains(&i) {
+                continue;
+            }
+            card_matches += 1;
+        }
+        total_matches += card_matches;
+    }
+    //println!("Total points: {points}");
     Ok(())
+}
+
+fn construct_sets(mut line: String, demo: bool) -> (HashSet<u32>, HashSet<u32>) {
+    // remove duplicated whitespaces (otherwise empty strings may be contained in the vector)
+    line = line.replace("   ", " ");
+    line = line.replace("  ", " ");
+    let chunks = line.split(' ').collect::<Vec<&str>>();
+    let mut winning_numbers = HashSet::new();
+    let mut owned_numbers = HashSet::new();
+    // filter out empty strings
+
+    // construct sets
+    for i in 2..chunks.len() {
+        // we skip the first two chunks because they are something like //Card 1:
+        // comment in this code for the example
+        if demo {
+            if (2..=6).contains(&i) {
+                winning_numbers.insert(chunks[i].parse::<u32>().unwrap());
+            } else if (8..=15).contains(&i) {
+                owned_numbers.insert(chunks[i].parse::<u32>().unwrap());
+            }
+        } else {
+            if (2..=11).contains(&i) {
+                winning_numbers.insert(chunks[i].parse::<u32>().unwrap());
+            } else if (13..=37).contains(&i) {
+                owned_numbers.insert(chunks[i].parse::<u32>().unwrap());
+            }
+        }
+    }
+    (winning_numbers, owned_numbers)
 }
 
 //fn remove_empty(chunks: &mut Vec<&str>) {
