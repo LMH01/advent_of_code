6a4770331cd2fd6f3ef20fb1dea1657856250a88
LMH01 <louis.haensch01@gmail.com>
2023-12-05 23:23:54 +0100

    fixed y2022 d04 p1

--- 2022/04/src/04.rs
+++ 2022/04/src/04.rs
@@ -3,11 +3,24 @@ use adventofcode_lmh01_lib::numbers_from_string;
 aoc::parts!(1, 2);
 
 pub fn part_1(input: aoc::Input) -> impl ToString {
-    calculate(input.as_lines(), true)
+    let mut i = 0;
+    for line in input {
+        let numbers = numbers_from_string(&line);
+        let a0 = numbers[0];
+        let a1 = numbers[1];
+        let a: (u32, u32) = (a0, a1);
+        let b0 = numbers[2];
+        let b1 = numbers[3];
+        let b: (u32, u32) = (b0, b1);
+        if fully_contained(a, b) {
+            i += 1;
+        }
+    }
+    println!("To reconsider: {i}");
+    i
 }
 
 pub fn part_2(input: aoc::Input) -> impl ToString {
-    // TODO currently broken - fix
     calculate(input.as_lines(), false)
 }
 
@@ -34,7 +47,7 @@ fn calculate(content: &[&str], part1: bool) -> i32 {
 }
 
 fn fully_contained(a: (u32, u32), b: (u32, u32)) -> bool {
-    if a.0 >= b.0 && a.1 <= b.1 {
+    if a.0 >= b.0 && a.1 <= b.1 || b.0 >= a.0 && b.1 <= a.1 {
         return true;
     }
     false
@@ -45,4 +58,20 @@ fn overlaps(a: (u32, u32), b: (u32, u32)) -> bool {
         return true;
     }
     false
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::fully_contained;
+
+    
+    #[test]
+    fn test_fully_contained() {
+        assert!(!fully_contained((2, 4), (6, 8)));
+        assert!(!fully_contained((2, 3), (4, 5)));
+        assert!(!fully_contained((5, 7), (7, 9)));
+        assert!(fully_contained((2, 8), (3, 7)));
+        assert!(fully_contained((6, 6), (4, 6)));
+        assert!(!fully_contained((2, 6), (4, 8)));
+    }
 }
