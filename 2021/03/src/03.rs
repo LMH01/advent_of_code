db3c57f098239ffbabcfd095f972af1963171098
LMH01 <louis.haensch01@gmail.com>
2023-12-05 22:42:36 +0100

    fixed y2021 d3

--- 2021/03/src/03.rs
+++ 2021/03/src/03.rs
@@ -1,52 +1,48 @@
-use adventofcode_lmh01_lib::transform_vec;
-
 aoc::parts!(1, 2);
 
 pub fn part_1(input: aoc::Input) -> impl ToString {
-    todo!("needs fix");
-    0
-    //let vec = transform_vec(input.as_lines());
-    //let mut gamma = String::new();
-    //let mut epsilon = String::new();
-    //for line in vec {
-    //    let mut zeros = 0;
-    //    let mut ones = 0;
-    //    for character in line.chars() {
-    //        match character {
-    //            '0' => zeros += 1,
-    //            '1' => ones += 1,
-    //            _ => (),
-    //        }
-    //    }
-    //    match zeros.cmp(&ones) {
-    //        std::cmp::Ordering::Greater => {
-    //            gamma.push('0');
-    //            epsilon.push('1');
-    //        }
-    //        std::cmp::Ordering::Less => {
-    //            gamma.push('1');
-    //            epsilon.push('0');
-    //        }
-    //        std::cmp::Ordering::Equal => (),
-    //    }
-    //}
-    //let gamma_as_int = isize::from_str_radix(&gamma, 2).unwrap();
-    //let epsilon_as_int = isize::from_str_radix(&epsilon, 2).unwrap();
-    //println!("Gamma rate: {}\nEpsilon rate: {}", gamma, epsilon);
-    //println!(
-    //    "Gamma rate: {}\nEpsilon rate: {}",
-    //    gamma_as_int, epsilon_as_int
-    //);
-    //println!("Result: {}", gamma_as_int * epsilon_as_int);
-    //gamma_as_int * epsilon_as_int
+    let vec = transform_vec(input.as_lines());
+    let mut gamma = String::new();
+    let mut epsilon = String::new();
+    for line in vec {
+        let mut zeros = 0;
+        let mut ones = 0;
+        for character in line.chars() {
+            match character {
+                '0' => zeros += 1,
+                '1' => ones += 1,
+                _ => (),
+            }
+        }
+        match zeros.cmp(&ones) {
+            std::cmp::Ordering::Greater => {
+                gamma.push('0');
+                epsilon.push('1');
+            }
+            std::cmp::Ordering::Less => {
+                gamma.push('1');
+                epsilon.push('0');
+            }
+            std::cmp::Ordering::Equal => (),
+        }
+    }
+    let gamma_as_int = isize::from_str_radix(&gamma, 2).unwrap();
+    let epsilon_as_int = isize::from_str_radix(&epsilon, 2).unwrap();
+    println!("Gamma rate: {}\nEpsilon rate: {}", gamma, epsilon);
+    println!(
+        "Gamma rate: {}\nEpsilon rate: {}",
+        gamma_as_int, epsilon_as_int
+    );
+    println!("Result: {}", gamma_as_int * epsilon_as_int);
+    gamma_as_int * epsilon_as_int
 }
 
 pub fn part_2(input: aoc::Input) -> impl ToString {
-    let ogr = get_raiting(input.as_lines(), 0, SystemRaiting::OxygenGeneratorRating)
+    let ogr = get_raiting(create_vec(input.as_lines()), 0, SystemRaiting::OxygenGeneratorRating)
         .get(0)
         .unwrap()
         .clone();
-    let csr = get_raiting(input.as_lines(), 0, SystemRaiting::CO2ScrubberRating)
+    let csr = get_raiting(create_vec(input.as_lines()), 0, SystemRaiting::CO2ScrubberRating)
         .get(0)
         .unwrap()
         .clone();
@@ -77,11 +73,11 @@ enum Operation {
 /// * 'index' - At what index the number should be searched (Should always be called with 0 as
 /// start value)
 /// * 'system_type' - Determines what calculation should be used to determine the raiting
-fn get_raiting(vec: &[&str], index: usize, system_type: SystemRaiting) -> Vec<String> {
+fn get_raiting(vec: Vec<String>, index: usize, system_type: SystemRaiting) -> Vec<String> {
     let mut accepted: Vec<String> = Vec::new(); // this vector will contain all lines that are still valid for the system
     let mut zeros = 0;
     let mut ones = 0;
-    for line in vec {
+    for line in &vec {
         // count how many zeros and ones are at the nth position
         match line.chars().nth(index).unwrap_or('-') {
             '0' => zeros += 1,
@@ -112,7 +108,7 @@ fn get_raiting(vec: &[&str], index: usize, system_type: SystemRaiting) -> Vec<St
             || (line.chars().nth(index).unwrap_or('-').eq(&'1')
                 && matches!(operation, Operation::KeepOnes))
         {
-            accepted.push(String::from(*line));
+            accepted.push(String::from(line));
         }
     }
     // ''accepted' vector has been filled with the remaining values
@@ -121,8 +117,27 @@ fn get_raiting(vec: &[&str], index: usize, system_type: SystemRaiting) -> Vec<St
     if accepted.len() == 1 {
         accepted
     } else {
-        // TODO fix
-        //get_raiting(accepted.as_slice(), index + 1, system_type)
-        !todo!("needs fix");
+        get_raiting(accepted, index + 1, system_type)
     }
+}
+
+pub fn transform_vec(vec: &[&str]) -> Vec<String> {
+    let mut output: Vec<String> = vec![String::new(); vec.get(0).unwrap_or(&String::new().as_str()).len()];
+    for line in vec {
+        for (index, character) in line.chars().enumerate() {
+            output
+                .get_mut(index)
+                .unwrap_or(&mut String::new())
+                .push(character);
+        }
+    }
+    output
+}
+
+fn create_vec(input: &[&str]) -> Vec<String> {
+    let mut vec = Vec::new();
+    for line in input {
+        vec.push(line.to_string());
+    }
+    vec
 }
