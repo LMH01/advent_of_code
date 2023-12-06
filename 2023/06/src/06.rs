2068a2a945b0ecd44c166bfb7777a728469c44b4
LMH01 <louis.haensch01@gmail.com>
2023-12-06 15:09:42 +0100

    y2023 d06 p1

---
+++ 2023/06/src/06.rs
@@ -0,0 +1,72 @@
+use adventofcode_lmh01_lib::numbers_from_string;
+
+aoc::parts!(1);
+
+fn part_1(input: aoc::Input) -> impl ToString {
+    let times = numbers_from_string(input[0]);
+    let distances = numbers_from_string(input[1]);
+    let mut races = Vec::new();
+    for i in 0..times.len() {
+        races.push(Race::new(times[i], distances[i]));
+    }
+    let mut result = 0;
+    for race in races {
+        if result == 0 {
+            result = race.possible_ways();
+        } else {
+            result *= race.possible_ways();
+        }
+    }
+    result
+}
+
+// fn part_2(input: aoc::Input) -> impl ToString {
+//     0
+// }
+
+struct Race {
+    duration: u32,
+    distance: u32,
+}
+
+impl Race {
+
+    fn new(duration: u32, distance: u32) -> Self {
+        Self {
+            duration,
+            distance,
+        }
+    }
+
+    fn possible_ways(&self) -> u32 {
+        let range_vec = construct_range_vec(self.duration);
+        let mut possible_ways = 0;
+        for x in range_vec {
+            if x > self.distance {
+                possible_ways += 1;
+            }
+        }
+        possible_ways
+    }
+}
+
+fn construct_range_vec(duration: u32) -> Vec<u32> {
+    let mut vec = Vec::new();
+    for i in 0..=duration {
+        vec.push((duration-i)*i);
+    }
+    vec
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Race;
+
+
+    #[test]
+    fn test_possible_ways() {
+        assert_eq!(Race::new(7, 9).possible_ways(), 4);
+        assert_eq!(Race::new(15, 40).possible_ways(), 8);
+        assert_eq!(Race::new(30, 200).possible_ways(), 9);
+    }
+}
