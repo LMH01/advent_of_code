7baaf6c86bbcd9d8a4635d70b1b08b9aa8934cc4
LMH01 <louis.haensch01@gmail.com>
2023-12-08 11:16:03 +0100

    y2023 d08 p2 - attempt 1 - bruteforce takes too long

--- 2023/08/src/08.rs
+++ 2023/08/src/08.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-aoc::parts!(1);
+aoc::parts!(1, 2);
 
 fn part_1(input: aoc::Input) -> impl ToString {
     let mut steps = 0;
@@ -28,9 +28,46 @@ fn part_1(input: aoc::Input) -> impl ToString {
     steps
 }
 
-// fn part_2(input: aoc::Input) -> impl ToString {
-//     0
-// }
+//fn part_2(input: aoc::Input) -> impl ToString {
+//    let mut steps = 0;
+//    let parsed = parse(input);
+//    // store the current nodes
+//    let mut currents = Vec::new();
+//    // find all nodes that end with 'A'
+//    for entry in &parsed.1 {
+//        if entry.0.ends_with('A') {
+//            currents.push(*entry.0);
+//        }
+//    }
+//    println!("Starting at {} nodes", currents.len());
+//    // the start nodes have been constructed
+//    'outer: loop {
+//        'inner: for c in parsed.0.chars() {
+//            for current in currents.iter_mut().enumerate() {
+//                match c {
+//                    'L' => *current.1 = parsed.1.get(current.1).unwrap().0.as_str(),
+//                    'R' => *current.1 = parsed.1.get(current.1).unwrap().1.as_str(),
+//                    _ => (),
+//                }
+//            }
+//            steps += 1;
+//            // check if all new currents end with 'Z'
+//            let mut currents_correct = 0;
+//            for current in &currents {
+//                if !current.ends_with('Z') {
+//                    if currents_correct > 2 {
+//                        println!("{currents_correct}");
+//                    }
+//                    continue 'inner;
+//                }
+//                currents_correct += 1;
+//            }
+//            // when we are here we know that all current nodes end with 'Z', so we are finished
+//            break 'outer;
+//        }
+//    }
+//    steps
+//}
 
 /// Tuple.0 = navigation
 /// Tuple.1 = constructed hash map
